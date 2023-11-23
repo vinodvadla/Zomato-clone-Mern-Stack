97a56f2a660c2ef5809fe52990a34c7c3b47693a
vinodvadla <vinodvinod0979@gmail.com>
2023-11-23 22:02:40 +0530

    added static files

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -17,6 +17,7 @@ const cartrouter = require('../Routes/cartRoutes')
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('../public'))
 app.use("/", userRouter);
 app.use("/locations", locationRouter);
 app.use("/mealtypes", mealRouter);
